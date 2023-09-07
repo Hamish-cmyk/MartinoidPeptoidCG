2122c941639b28263159b1e72c9169f4b478c5ce
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2023-09-07 12:09:45 +0100

    Update __init__.py

--- Martinoid/__init__.py
+++ Martinoid/__init__.py
@@ -7,8 +7,8 @@ Created on Thu Sep  7 10:40:29 2023
 
 
 # import submodules
-#from Martinoid import _martinoid_data
-#from Martinoid.Martinoid2 import *
+from Martinoid import _martinoid_data
+from Martinoid.Martinoid2 import *
 
 
 
